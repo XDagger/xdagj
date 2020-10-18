93c1f92a257e57c7bee1b07b5577608900d3a513
holt666 <holt666@gmx.com>
2020-10-18 17:59:14 +0800

    fix unit test issue

--- src/test/java/io/xdag/basic/Address2Hash.java
+++ src/test/java/io/xdag/basic/Address2Hash.java
@@ -28,6 +28,7 @@ import org.spongycastle.util.encoders.Hex;
 
 import static io.xdag.utils.BasicUtils.address2Hash;
 import static io.xdag.utils.BasicUtils.hash2Address;
+import static org.junit.Assert.assertTrue;
 
 public class Address2Hash {
 
@@ -36,7 +37,7 @@ public class Address2Hash {
         String news = "42cLWCMWZDKPZM8WJfpmI7Lbe3p83U2l";
         String originhash = "4aa1ab5742feb010a54ddd7c7a7bdbb22366fa2516cf648f32641623580b67e3";
         byte[] hash1 = Hex.decode(originhash);
-        assert (hash2Address(hash1).equals(news));
+        assertTrue(hash2Address(hash1).equals(news));
     }
 
     @Test
@@ -44,6 +45,6 @@ public class Address2Hash {
         String news = "42cLWCMWZDKPZM8WJfpmI7Lbe3p83U2l";
         String originhashlow = "0000000000000000a54ddd7c7a7bdbb22366fa2516cf648f32641623580b67e3";
         byte[] hashlow = address2Hash(news);
-        assert (Hex.toHexString(hashlow).equals(originhashlow));
+        assertTrue(Hex.toHexString(hashlow).equals(originhashlow));
     }
 }
