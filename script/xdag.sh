69fe666092b5f5db42e83e6147b56c6c39bdfc49
holt666 <holt666@gmx.com>
2021-05-21 21:03:41 +0800

    add xdag start shell

---
+++ script/xdag.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+XDAG_VERSION="0.4.2"
+XDAG_JARNAME="xdagj-${XDAG_VERSION}-shaded.jar"
+XDAG_OPTS="-t"
+
+# Linux Java Home
+JAVA_HOME="/usr/local/java/"
+
+# MacOS Java Home
+#JAVA_HOME=/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home/
+
+# default JVM options
+JAVA_OPTS="--enable-preview -server -Xms1g -Xmx1g"
+
+# start kernel
+nohup java ${JAVA_OPTS} -cp ${XDAG_JARNAME} io.xdag.Bootstrap "$@"
