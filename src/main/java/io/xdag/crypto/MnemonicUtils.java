70b5f3909c5a5edd2dfe9c9c23dfafff1503aac4
holt666 <holt666@gmx.com>
2021-11-15 20:59:26 +0800

    refactor

--- src/main/java/io/xdag/crypto/MnemonicUtils.java
+++ src/main/java/io/xdag/crypto/MnemonicUtils.java
@@ -24,7 +24,6 @@
 
 package io.xdag.crypto;
 
-import static io.xdag.crypto.Hash.sha256;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.BufferedReader;
@@ -40,6 +39,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
+import org.apache.tuweni.crypto.Hash;
 import org.bouncycastle.crypto.digests.SHA512Digest;
 import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
 import org.bouncycastle.crypto.params.KeyParameter;
@@ -265,7 +265,7 @@ public class MnemonicUtils {
     public static byte calculateChecksum(byte[] initialEntropy) {
         int ent = initialEntropy.length * 8;
         byte mask = (byte) (0xff << 8 - ent / 32);
-        Bytes32 bytes = sha256(Bytes.wrap(initialEntropy));
+        Bytes32 bytes = Hash.sha2_256(Bytes.wrap(initialEntropy));
 
         return (byte) (bytes.get(0) & mask);
     }
