6276666480845d7f3d331eb22e1c1d8ec523386d
holt666 <holt666@gmx.com>
2022-01-26 15:14:28 +0800

    modify vm opts for jdk17

--- script/xdag.sh
+++ script/xdag.sh
@@ -11,7 +11,7 @@ XDAG_OPTS="-t"
 #JAVA_HOME=/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home/
 
 # default JVM options
-JAVA_OPTS="-server -Xms1g -Xmx1g"
+JAVA_OPTS="--add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -server -Xms1g -Xmx1g"
 
 # start kernel
 java ${JAVA_OPTS} -cp .:${XDAG_JARNAME} io.xdag.Bootstrap "$@"
