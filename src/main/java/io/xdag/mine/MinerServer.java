fdce026490b0e93ead8e11ec7ec565119bf848d7
holt666 <holt666@gmx.com>
2022-09-07 10:55:11 +0800

    reactor pool and node netty handler

--- src/main/java/io/xdag/mine/MinerServer.java
+++ src/main/java/io/xdag/mine/MinerServer.java
@@ -29,26 +29,19 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.DefaultMessageSizeEstimator;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.epoll.EpollEventLoopGroup;
-import io.netty.channel.epoll.EpollServerSocketChannel;
-import io.netty.channel.kqueue.KQueueEventLoopGroup;
-import io.netty.channel.kqueue.KQueueServerSocketChannel;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.NettyRuntime;
 import io.xdag.Kernel;
+import io.xdag.utils.NettyUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.SystemUtils;
 
 @Slf4j
 public class MinerServer {
     private Kernel kernel;
     private EventLoopGroup bossGroup;
     private EventLoopGroup workerGroup;
-    private final int workerThreadPoolSize = NettyRuntime.availableProcessors() * 4;
     private ChannelFuture channelFuture;
-    private boolean isListening = false;
+    private final int workerThreadPoolSize = NettyRuntime.availableProcessors() * 4;
 
     public MinerServer(Kernel kernel) {
         this.kernel = kernel;
@@ -58,62 +51,31 @@ public class MinerServer {
         start(kernel.getConfig().getPoolSpec().getPoolIp(), kernel.getConfig().getPoolSpec().getPoolPort());
     }
 
-    public ServerBootstrap nativeEventLoopGroup() {
-        ServerBootstrap bootstrap = new ServerBootstrap();
-
-        if(SystemUtils.IS_OS_LINUX) {
-            bossGroup = new EpollEventLoopGroup();
-            workerGroup = new EpollEventLoopGroup(workerThreadPoolSize);
-        } else if(SystemUtils.IS_OS_MAC) {
-            bossGroup = new KQueueEventLoopGroup();
-            workerGroup = new KQueueEventLoopGroup(workerThreadPoolSize);
-
-        } else {
-            bossGroup = new NioEventLoopGroup();
-            workerGroup = new NioEventLoopGroup(workerThreadPoolSize);
-        }
-
-        bootstrap.group(bossGroup, workerGroup);
-
-        if(SystemUtils.IS_OS_LINUX) {
-            bootstrap.channel(EpollServerSocketChannel.class);
-        } else if(SystemUtils.IS_OS_MAC) {
-            bootstrap.channel(KQueueServerSocketChannel.class);
-        } else {
-            bootstrap.channel(NioServerSocketChannel.class);
-        }
-
-        return bootstrap;
-    }
-
     public void start(String ip, int port) {
         try {
-            ServerBootstrap bootstrap = nativeEventLoopGroup();
-            bootstrap.childOption(ChannelOption.TCP_NODELAY, true);
-            bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
-            bootstrap.childOption(ChannelOption.MESSAGE_SIZE_ESTIMATOR, DefaultMessageSizeEstimator.DEFAULT);
-            bootstrap.childOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, kernel.getConfig().getPoolSpec().getConnectionTimeout());
-            bootstrap.handler(new LoggingHandler());
-            bootstrap.childHandler(new MinerChannelInitializer(kernel, true));
-            channelFuture = bootstrap.bind(ip, port).sync();
-            isListening = true;
-            log.info("Start Listening The Pool, Host:[{}:{}]", ip, port);
+            ServerBootstrap b = NettyUtils.nativeEventLoopGroup(bossGroup, workerGroup, workerThreadPoolSize);
+            b.childOption(ChannelOption.TCP_NODELAY, true);
+            b.childOption(ChannelOption.SO_KEEPALIVE, true);
+            b.childOption(ChannelOption.MESSAGE_SIZE_ESTIMATOR, DefaultMessageSizeEstimator.DEFAULT);
+            b.childOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, kernel.getConfig().getPoolSpec().getConnectionTimeout());
+            b.handler(new LoggingHandler());
+            b.childHandler(new MinerChannelInitializer(kernel, true));
+            channelFuture = b.bind(ip, port).sync();
+            log.info("Xdag Pool start host:[{}:{}]", ip, port);
         } catch (Exception e) {
-            log.error("Miner Server Error: {}.", e.getMessage(), e);
+            log.error("Xdag Pool start error:{}.", e.getMessage(), e);
         }
     }
 
     public void close() {
-        if (isListening && channelFuture != null && channelFuture.channel().isOpen()) {
+        if (channelFuture != null && channelFuture.channel().isOpen()) {
             try {
-                log.debug("Closing Miner Server...");
                 channelFuture.channel().close().sync();
                 workerGroup.shutdownGracefully();
                 bossGroup.shutdownGracefully();
-                isListening = false;
-                log.info("Miner Server Closed.");
+                log.info("Xdag Pool closed.");
             } catch (Exception e) {
-                log.error("Problems Closing Miner Server Channel: {}", e.getMessage(), e);
+                log.error("Xdag Pool close error:{}", e.getMessage(), e);
             }
         }
     }
