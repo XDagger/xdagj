1a24ef06471f3f45776e2209f3724d3943c2ffc0
q424492629 <424492629@qq.com>
2022-09-30 11:29:35 +0800

    fix close bug

--- src/main/java/io/xdag/net/XdagServer.java
+++ src/main/java/io/xdag/net/XdagServer.java
@@ -29,12 +29,16 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.DefaultMessageSizeEstimator;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.kqueue.KQueueEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.NettyRuntime;
 import io.xdag.Kernel;
 import io.xdag.net.handler.XdagChannelInitializer;
 import io.xdag.utils.NettyUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.SystemUtils;
 
 @Slf4j
 public class XdagServer {
@@ -54,6 +58,17 @@ public class XdagServer {
 
     public void start(String ip, int port) {
         try {
+            if(SystemUtils.IS_OS_LINUX) {
+                bossGroup = new EpollEventLoopGroup();
+                workerGroup = new EpollEventLoopGroup(workerThreadPoolSize);
+            } else if(SystemUtils.IS_OS_MAC) {
+                bossGroup = new KQueueEventLoopGroup();
+                workerGroup = new KQueueEventLoopGroup(workerThreadPoolSize);
+
+            } else {
+                bossGroup = new NioEventLoopGroup();
+                workerGroup = new NioEventLoopGroup(workerThreadPoolSize);
+            }
             ServerBootstrap b = NettyUtils.nativeEventLoopGroup(bossGroup, workerGroup, workerThreadPoolSize);
             b.childOption(ChannelOption.TCP_NODELAY, true);
             b.childOption(ChannelOption.SO_KEEPALIVE, true);
@@ -65,6 +80,9 @@ public class XdagServer {
             channelFuture = b.bind(ip, port).sync();
         } catch (Exception e) {
             log.error("Xdag Node start error:{}.", e.getMessage(), e);
+        }finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
         }
     }
 
@@ -72,10 +90,10 @@ public class XdagServer {
         if (channelFuture != null && channelFuture.channel().isOpen()) {
             try {
                 channelFuture.channel().close().sync();
-                workerGroup.shutdownGracefully();
-                bossGroup.shutdownGracefully();
-                workerGroup.terminationFuture().sync();
-                bossGroup.terminationFuture().sync();
+//                workerGroup.shutdownGracefully();
+//                bossGroup.shutdownGracefully();
+//                workerGroup.terminationFuture().sync();
+//                bossGroup.terminationFuture().sync();
                 log.debug("Xdag Node closed.");
             } catch (Exception e) {
                 log.error("Xdag Node close error:{}", e.getMessage(), e);
