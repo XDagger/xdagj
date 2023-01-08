892da173858be8f7722439e92c6997533ba44512
cherry1603 <565993183@qq.com>
2023-01-08 19:14:17 +0800

    fix snapshotj

--- src/main/java/io/xdag/db/SnapshotJ.java
+++ src/main/java/io/xdag/db/SnapshotJ.java
@@ -128,7 +128,7 @@ public class SnapshotJ extends RocksdbKVSource {
         snapshotSource.put(new byte[]{SNAPSHOT_PRESEED}, preSeed);
     }
 
-    public void saveSnapshotToIndex(BlockStore blockStore, List<KeyPair> keys,long snapshotTime,AddressStore addressStore) {
+    public void saveSnapshotToIndex(BlockStore blockStore, List<KeyPair> keys,long snapshotTime) {
         try (RocksIterator iter = getDb().newIterator()) {
             for (iter.seekToFirst(); iter.isValid(); iter.next()) {
                 if (iter.key()[0] == 0x30) {
@@ -159,10 +159,6 @@ public class SnapshotJ extends RocksdbKVSource {
                                         break;
                                     }
                                 }
-                                UInt64 balance = addressStore.getBalanceByAddress(pubKey);
-                                balance = balance.add(blockInfo.getAmount());
-                                addressStore.updateBalance(pubKey,balance);
-                                blockInfo.setAmount(UInt64.ZERO);
                             } else {    //Verify signature
                                 Block block = new Block(new XdagBlock(snapshotInfo.getData()));
                                 SECPSignature outSig = block.getOutsig();
